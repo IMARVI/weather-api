6a7188819f27dfca2d61cda30a2ea364a1033cea
isaiasmarvi <isaiasmarvi@gmail.com>
2017-11-13 15:15:07 -0600

    registrar mails diferentes

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -2,4 +2,25 @@
 
 module.exports = function(Usuario) {
 
+  Usuario.disponibilidad = function(email, cb) {
+    console.log(email);
+    var resp = [];
+    Usuario.find({where : {email:email}}, function(err,response){
+      console.log(response.length);
+      if(response.length>0){
+        cb(null,false);
+      }else{
+        cb(null,true);
+      }
+    });
+  }
+
+  Usuario.remoteMethod('disponibilidad', {
+        accepts: {arg: 'email',
+        type: 'string',
+        required: true
+      },
+        returns: {arg: 'disponible', type: 'boolean'},
+        http: {path:'/disponibilidad', verb: 'get'}
+  });
 };
